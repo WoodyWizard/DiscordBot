b629cdc6f6724e37fb73b2e092c736eddd730c17
WoodyWizard <pingbrooo@gmail.com>
2022-01-18 17:04:51 +0200

    fix get_first_word bug

--- src/lib.rs
+++ src/lib.rs
@@ -16,17 +16,17 @@
 
 
 pub fn get_first_word(line: &str) -> &str {
-
 let mut index = 0;
-    for i in line.chars() {
 
-        if i == ' ' {
-            return &line[0..index];
+    if line.get(0..1).unwrap() == "!" {
+        for i in line.chars() {
+            if i == ' ' {
+                return &line[0..index];
+            } else { index = index + 1; }
         }
-        index = index + 1;
     }
 
-return line;
+return &line[0..line.len()];
 }
 
 
